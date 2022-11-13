b6418b4e281aef51d894d474f241e0d915699c7e
hamishliu <hamish.mc.liu@gmail.com>
2022-11-14 00:39:56 +0900

    added cards

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from "react";
+import CardItem from "./CardItem";
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out these locations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+            src="/images/img-9.jpg"
+            text="Waterfall deep in the Amazon"
+            label='Adventure'
+            path='/services'
+            />
+                        <CardItem
+            src="/images/img-8.jpg"
+            text="Camel trekking across the Sahara"
+            label='Luxury'
+            path='/services'
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+            src="/images/img-6.jpg"
+            text="Travel through the metropolis which is Tokyo city"
+            label='Luxury'
+            path='/services'
+            />
+                        <CardItem
+            src="/images/img-4.jpg"
+            text="Enjoy life on a private island"
+            label='Luxury'
+            path='/services'
+            />
+                        <CardItem
+            src="/images/img-7.jpg"
+            text="Climb mountains and see views that no one has ever seen"
+            label='Adventure'
+            path='/services'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
